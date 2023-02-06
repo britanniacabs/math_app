631a568e49c01957363e150ce5ac4fcde07d1ee5
britanniacabs <britanniacabs@gmail.com>
2023-02-06 17:09:50 +0000

    Update AddNumbers.js

--- src/components/AddNumbers.js
+++ src/components/AddNumbers.js
@@ -1,38 +1,39 @@
 // AddNumbers.js
-import React, { useState } from "react"
-import "./AddNumbers.css"
+import React, { useState } from "react";
+import "./AddNumbers.css";
 
 function AddNumbers() {
-  const [num1, setNum1] = useState(Math.floor(Math.random() * 100))
-  const [num2, setNum2] = useState(Math.floor(Math.random() * 100))
-  const [answer, setAnswer] = useState(0)
-  const [correct, setCorrect] = useState(0)
-  const [incorrect, setIncorrect] = useState(0)
-  const [isCorrect, setIsCorrect] = useState(null)
+  const [num1, setNum1] = useState(Math.floor(Math.random() * 100));
+  const [num2, setNum2] = useState(Math.floor(Math.random() * 100));
+  const [answer, setAnswer] = useState(null);
+  const [correct, setCorrect] = useState(0);
+  const [incorrect, setIncorrect] = useState(0);
+  const [isCorrect, setIsCorrect] = useState(null);
 
   const handleAnswerChange = (event) => {
-    setAnswer(event.target.value)
-    setIsCorrect(null)
-  }
+    setAnswer(event.target.value);
+    setIsCorrect(null);
+  };
 
   const checkAnswer = (event) => {
-    const numericalAnswer = Number(answer)
+    const numericalAnswer = Number(answer);
     if (event.key === "Enter" || event.type === "click") {
       if (num1 + num2 === numericalAnswer) {
-        setCorrect(correct + 1)
-        setNum1(Math.floor(Math.random() * 100))
-        setNum2(Math.floor(Math.random() * 100))
-        setAnswer("")
-        setIsCorrect(true)
+        setCorrect(correct + 1);
+        setNum1(Math.floor(Math.random() * 100));
+        setNum2(Math.floor(Math.random() * 100));
+        setAnswer("");
+        setIsCorrect(true);
       } else {
-        setIncorrect(incorrect + 1)
-        setIsCorrect(false)
+        setIncorrect(incorrect + 1);
+        setIsCorrect(false);
       }
     }
-  }
+  };
 
   return (
     <div className="container">
+      <H1>Yursa Math App</H1>
       <p className="question">
         {num1} + {num2} ={" "}
       </p>
@@ -51,7 +52,7 @@ function AddNumbers() {
       <p>Correct answers: {correct}</p>
       <p>Incorrect answers: {incorrect}</p>
     </div>
-  )
+  );
 }
 
-export default AddNumbers
+export default AddNumbers;
