51b2dd51b24d34fd0ba2c2e46b85cb099b49d2d4
britanniacabs <britanniacabs@gmail.com>
2023-02-08 18:52:26 +0000

    App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 // App.js
 import React from "react";
-import AddNumbers from "./components/AddNumbers";
-import withAuth from "./components/WithAuth";
+//import AddNumbers from "./components/AddNumbers";
+//import withAuth from "./components/WithAuth";
 
 import Register from "./components/Register";
 import Navbar from "./components/NavBar";
 import Login from "./components/Login";
-const AuthenticatedAddNumbers = withAuth(AddNumbers);
+//const AuthenticatedAddNumbers = withAuth(AddNumbers);
 
 function App() {
   return (
