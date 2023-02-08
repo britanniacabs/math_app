ceb5baceee922cca402fa786cd8f7be11b412a69
britanniacabs <britanniacabs@gmail.com>
2023-02-08 18:58:37 +0000

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,13 +7,13 @@ const Navbar = () => {
         <span className="text-white text-2xl font-bold">Yusra Math App</span>
       </div>
       <div className="flex items-center">
-        <a href="#" className="text-white px-4 hover:bg-indigo-400">
+        <a href="./" className="text-white px-4 hover:bg-indigo-400">
           Home
         </a>
-        <a href="#" className="text-white px-4 hover:bg-indigo-400">
+        <a href="./" className="text-white px-4 hover:bg-indigo-400">
           About
         </a>
-        <a href="#" className="text-white px-4 hover:bg-indigo-400">
+        <a href="./" className="text-white px-4 hover:bg-indigo-400">
           Contact
         </a>
       </div>
